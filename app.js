1690d9ecb27707198c15780dd78974061eb983a2
KoGaYoung <4723515@naver.com>
2024-01-03 00:00:37 +0900

    server 배포

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 8000;
 
 // http method 중 ged으로 host:port를 보내면 실행되는 라우트
 app.get('/', (req, res) => {
-    res.send('hello, world');
+    res.send('hello, world!!!!!');
 });
 
-app.get('/', (req, res) => {
-    res.send('hello, world');
+app.get('/login', (req, res) => {
+    res.send('hello, login');
 }); 
 
 app.listen(port, () => {
